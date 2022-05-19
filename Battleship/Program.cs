15287771ebfb4b97bb81c9456b67039f5bcef9d2
Cassius Peter <Cassius.Peter@Dynisco.com>
2022-05-19 14:28:10 -0400

    Saving initial work before attempting a Gitignore

---
+++ Battleship/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] water = new char[10,10];
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    water[i, j] = '/';
+                    Console.Write("." + water[i, j] + ".");
+                }
+                Console.WriteLine("");
+            }
+            
+            
+        }
+    }
+}
