67640058b40a394e811d2546fb663844fb8b6474
Cassius Peter <Cassius.Peter@Dynisco.com>
2022-06-06 16:19:32 -0400

    Battleship Version 1.1 Prettied up display of board

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -7,12 +7,21 @@ namespace Battleship
         static void Main(string[] args)
         {
             char[,] water = new char[10,10];
+            //char[] Alpha = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
             for(int i = 0; i < 10; i++)
             {
+                if (i == 0)
+                {
+                    Console.WriteLine("   A  B  C  D  E  F  G  H  I  J");
+                }
                 for(int j = 0; j < 10; j++)
                 {
-                    water[i, j] = '/';
-                    Console.Write("." + water[i, j] + ".");
+                    if(j == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                    water[i, j] = '^';
+                    Console.Write(" " + water[i, j] + " ");
                 }
                 Console.WriteLine("");
             }
