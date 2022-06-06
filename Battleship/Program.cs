1273eff31ae3cff78e8322671c119ff33c3d48ed
Cassius Peter <Cassius.Peter@Dynisco.com>
2022-06-06 16:40:41 -0400

    End of Day saving Added beginning of Fire method. Having trouble parsing string value into ints. Look up better ways of parsing strings to ints, and splitting strings. Note

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -7,7 +7,7 @@ namespace Battleship
         static void Main(string[] args)
         {
             char[,] water = new char[10,10];
-            //char[] Alpha = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+            char[] Alpha = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
             for(int i = 0; i < 10; i++)
             {
                 if (i == 0)
@@ -18,15 +18,27 @@ namespace Battleship
                 {
                     if(j == 0)
                     {
-                        Console.Write(i + " ");
+                        Console.Write(Alpha[i] + " ");
                     }
                     water[i, j] = '^';
                     Console.Write(" " + water[i, j] + " ");
                 }
                 Console.WriteLine("");
+                Console.WriteLine("Enter your shot");
+                string Target = Console.ReadLine();
+                Fire(Target);
             }
             
             
+        }
+
+        public static void Fire(string Input)
+        {
+            char Row = Input[0];
+            //char Col = Input[1];
+            int R = Row - 'A';
+            int temp = Input.Length;
+
         }
     }
 }
